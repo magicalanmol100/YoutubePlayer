19adc8921cfe69f45fb32cffead1194873ce05f7
unknown <magicalanmol100@gmail.com>
2020-01-24 10:37:56 +0530

    Updates

---
+++ precache-manifest.d38969860b75a7baff739f286fd770a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64b14a470818f924144b5a864888221",
+    "url": "/YouTubePlayer/index.html"
+  },
+  {
+    "revision": "a1eddee83f2aa20a5053",
+    "url": "/YouTubePlayer/static/css/main.97839f8b.chunk.css"
+  },
+  {
+    "revision": "2976c7db5432b225c17c",
+    "url": "/YouTubePlayer/static/js/2.7cecc4f3.chunk.js"
+  },
+  {
+    "revision": "a1eddee83f2aa20a5053",
+    "url": "/YouTubePlayer/static/js/main.a9a7e81d.chunk.js"
+  },
+  {
+    "revision": "8b2a0635bd2331211aba",
+    "url": "/YouTubePlayer/static/js/runtime~main.65eef49c.js"
+  }
+]);
